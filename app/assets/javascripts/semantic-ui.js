a12ccb6c2e15fef9c32684b60acc24576d90ec4a
Anastasia <anastasia.lyubich@gmail.com>
2017-02-09 20:56:40 +0600

    Fix delay problem in search system [2]

--- app/assets/javascripts/semantic-ui.js
+++ app/assets/javascripts/semantic-ui.js
@@ -18895,7 +18895,7 @@ $.api = $.fn.api = function(parameters) {
               }
               setTimeout(function() {
                 if( module.is.validResponse(response) ) {
-                  // module.request.resolveWith(context, [response, xhr]);
+                  module.request.resolveWith(context, [response, xhr]);
                 }
                 else {
                   module.request.rejectWith(context, [xhr, 'invalid']);
@@ -18917,7 +18917,7 @@ $.api = $.fn.api = function(parameters) {
               }
               setTimeout(function() {
                 if( module.is.abortedRequest(xhr) ) {
-                  module.request.rejectWith(context, [xhr, 'aborted', httpMessage]);
+                  // module.request.rejectWith(context, [xhr, 'aborted', httpMessage]);
                 }
                 else {
                   module.request.rejectWith(context, [xhr, 'error', status, httpMessage]);
